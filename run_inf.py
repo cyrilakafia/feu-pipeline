f83e90f4fbe75bbe2bc1c3fe46066fe808a866c0
Cyril Akafia <kwakucyril@gmail.com>
2024-05-01 16:06:33 -0400

    run_inf test

--- run_inf.py
+++ run_inf.py
@@ -1,5 +1,4 @@
 from inf import run_inference
-from pandas import DataFrame
 
 best_assigns, best_params = run_inference('../feu-with1231/sim1234_true.p', title='demo', device='cpu', iterations=5, seed=None)
 
