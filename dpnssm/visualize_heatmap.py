c4c7b97b91d2696de7db37b3bbba94274d5aac52
Cyril Akafia <kwakucyril@gmail.com>
2024-05-10 13:44:07 -0400

    print statements

--- dpnssm/visualize_heatmap.py
+++ dpnssm/visualize_heatmap.py
@@ -91,14 +91,14 @@ def viz_heatmap(run, iter):
     best_params = pd.DataFrame(average_params)
     best_params.columns = ['jump', 'phasicity']
     best_params.to_csv(f'outputs/sim{run}_best_params.csv')
-    print('Found best parameters. Saving to file to outputs/sim{run}_best_params.csv')
+    print(f'Found best parameters. Saving to file to outputs/sim{run}_best_params.csv')
 
     # rotate the df
     best_assigns = best_assigns.T
 
     # save best_assigns to csv file
     best_assigns.to_csv(f'outputs/sim{run}_best_assigns.csv')
-    print('Found best assigns. Saving to file to outputs/sim{run}_best_assigns.csv')
+    print(f'Found best assigns. Saving to file to outputs/sim{run}_best_assigns.csv')
 
     reorder = assigns.loc[min_dist_idx].sort_values().index
 
@@ -122,7 +122,7 @@ def viz_heatmap(run, iter):
         plt.savefig(f'outputs/sim{run}_assigns.png', dpi = 500)
         
     plt.close()
-    print('Saving cluster assignments heatmap to outputs/sim{run}_assigns.png')
+    print(f'Saving cluster assignments heatmap to outputs/sim{run}_assigns.png')
 
     # make params figure
     plt.scatter(average_params[:, 1] + 15, average_params[:, 0], s=20)
@@ -132,7 +132,7 @@ def viz_heatmap(run, iter):
     plt.tight_layout()
     plt.savefig(f'outputs/sim{run}_params.png', dpi = 500)
     plt.close()
-    print('Saving scatter plot of cluster parameters to outputs/sim{run}_params.png')
+    print(f'Saving scatter plot of cluster parameters to outputs/sim{run}_params.png')
 
 
     return best_assigns, best_params
