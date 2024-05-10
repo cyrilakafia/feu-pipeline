a0adbf127a247c67ecf82c5b1d1e2d99f6b0df6d
Cyril Akafia <kwakucyril@gmail.com>
2024-05-10 13:36:16 -0400

    Update example code

--- run_inf.py
+++ run_inf.py
@@ -1,17 +1,33 @@
 from inf import run_inference
+from dpnssm.prep import prep
+import os
 
-best_assigns, best_params = run_inference('/Users/csa46/Documents/Computation/functional encoding units/feu-with1231/sim1231_true.p', 
-                                          title='demo', 
-                                          device='cpu', 
-                                          iterations=5, 
-                                          concentration=1, 
-                                          max_clusters=20, 
-                                          timepoint=0, 
-                                          seed=None
-                                          )
 
-print(type(best_assigns))
-print(type(best_params))
+original_data = '/Users/csa46/Documents/Computation/functional encoding units/GUI/feu-app/test_data/array_30_200.pkl'
+
+# Preprocess the data
+preprocessed_data = 'outputs/processed_data.p'
+
+if not os.path.exists('outputs'):
+    os.makedirs('outputs')
+
+prep(original_data, preprocessed_data)
+
+# Run the inference process
+best_assigns, best_params = run_inference(
+                                        # '/Users/csa46/Documents/Computation/functional encoding units/feu-with1231/sim1231_true.p', 
+                                        preprocessed_data,
+                                        title='demo', 
+                                        device='cpu', 
+                                        iterations=5, 
+                                        concentration=1, 
+                                        max_clusters=20, 
+                                        timepoint=0, 
+                                        seed=None
+                                        )
+
+# print(type(best_assigns))
+# print(type(best_params))
 
 # <class 'pandas.core.frame.DataFrame'>
 # <class 'pandas.core.frame.DataFrame'>ex
