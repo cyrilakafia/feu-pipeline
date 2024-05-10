f4a754647de8c6d6eb9bf0f9314d3765885ddd01
Cyril Akafia <kwakucyril@gmail.com>
2024-05-10 12:09:37 -0400

    test_runs

--- run_inf.py
+++ run_inf.py
@@ -1,10 +1,18 @@
 from inf import run_inference
 
-best_assigns, best_params = run_inference('../feu-with1231/sim1234_true.p', title='demo', device='cpu', iterations=5, seed=None)
+best_assigns, best_params = run_inference('/Users/csa46/Documents/Computation/functional encoding units/feu-with1231/sim1231_true.p', 
+                                          title='demo', 
+                                          device='cpu', 
+                                          iterations=5, 
+                                          concentration=1, 
+                                          max_clusters=20, 
+                                          timepoint=0, 
+                                          seed=None
+                                          )
 
 print(type(best_assigns))
 print(type(best_params))
 
 # <class 'pandas.core.frame.DataFrame'>
-# <class 'pandas.core.frame.DataFrame'>
+# <class 'pandas.core.frame.DataFrame'>ex
 
