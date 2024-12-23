47311434a73f837ce33613bb192a8b9116714a63
Cyril Akafia <kwakucyril@gmail.com>
2024-12-23 13:33:57 -0500

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='feu',
-    version='0.0.12.2024.11.4',
+    version='0.12.23.2024',
     packages=find_packages(),
     install_requires=[
         'torch',
