f5aef20710dfcc8cb40cd9368cb74f99704ec6bd
Cyril Akafia <kwakucyril@gmail.com>
2026-01-19 14:40:29 -0500

    Update version number to 0.01.19.2026 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='feu',
-    version='0.09.14.2025',
+    version='0.01.19.2026',
     packages=find_packages(),
     install_requires=[
         'torch',
